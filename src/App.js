27f14d47115d7b0787896e303001218b7c08c4e0
Atharva Pardeshi <atharva.exe@gmail.com>
2022-06-22 08:18:53 +0530

    feat(s): write App class, add default monsters state for when offline, fetch random users, images and email addresses and pass monsters to CardList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstien",
+          id: 12345,
+        },
+        {
+          name: "Frank",
+          id: 12346,
+        },
+        {
+          name: "Stien",
+          id: 12347,
+        },
+        {
+          name: "Ken",
+          id: 12348,
+        },
+        {
+          name: "Ranken",
+          id: 12349,
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
