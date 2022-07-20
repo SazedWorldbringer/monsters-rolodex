199d5cf5e2dc2c4096f94348bcdde3c8136f243c
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-20 21:27:05 +0530

    feat: change image url

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,11 +4,15 @@ import "./card.styles.css";
 
 export const Card = (props) => (
   <div className="card-container">
-    <img
-      src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
-      alt="monster"
-    />
+    <img src={props.monster.images.md} alt={props.monster.name} />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
 );
+
+/* <img
+      src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
+      alt="monster"
+    />*/
+
+// fix the image
