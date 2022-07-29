cd1299897a766f16622dddce337e16183c79b4eb
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-29 18:08:55 +0530

    feat: align the app back to the course

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,15 +4,16 @@ import "./card.styles.css";
 
 export const Card = (props) => (
   <div className="card-container">
-    <img src={props.monster.images.md} alt={props.monster.name} />
+    <img
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      alt="monster"
+    />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
 );
 
-/* <img
-      src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
-      alt="monster"
-    />*/
-
-// fix the image
+/* 
+    images from the superhero api
+    <img src={props.monster.image} alt={props.monster.name} />
+*/
